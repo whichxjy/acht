4ee11e21999a19844e1cae014b68802fd8bca22e
whichxjy <whichxjy@gmail.com>
2020-01-28 09:00:36 +0800

    Convert tabs to spaces

--- acht/Logger.hpp
+++ acht/Logger.hpp
@@ -1,7 +1,7 @@
 #ifndef _LOGGER_HPP_
 #define _LOGGER_HPP_
 
-#include "SynchronousQueue.hpp"
+#include "SyncQueue.hpp"
 #include <string>
 #include <fstream>
 #include <memory>
@@ -15,231 +15,231 @@
 
 namespace acht {
 
-	class Logger {
-	public:
-		using LogRecord = std::string;
-		using LogMessage = std::string;
+    class Logger {
+    public:
+        using LogRecord = std::string;
+        using LogMessage = std::string;
  
-		/***********************************************************
-		 *  The level of the events to be tracked (or the level of
-		 *  messages to be sent).
-		 ***********************************************************/
-		enum class Level {
-			FATAL,
-			ERROR,
-			WARN,
-			INFO,
-			DEBUG
-		};
-		
-		/***********************************************************
-		 *  Get the instance of logger. It allows only a single 
-		 *  instance to be created. If the parameter "level" is
-		 *  different form logger's level, reset it.
-		 ***********************************************************/
-		static std::shared_ptr<Logger> getLogger(Level level = Level::DEBUG) {
-			if (myLogger == nullptr)
-				myLogger = std::shared_ptr<Logger>(new Logger(level));
-			else if (myLogger->myLevel != level)
-				myLogger->setLevel(level);
-			return myLogger;
-		}
+        /***********************************************************
+         *  The level of the events to be tracked (or the level of
+         *  messages to be sent).
+         ***********************************************************/
+        enum class Level {
+            FATAL,
+            ERROR,
+            WARN,
+            INFO,
+            DEBUG
+        };
+        
+        /***********************************************************
+         *  Get the instance of logger. It allows only a single 
+         *  instance to be created. If the parameter "level" is
+         *  different form logger's level, reset it.
+         ***********************************************************/
+        static std::shared_ptr<Logger> getLogger(Level level = Level::DEBUG) {
+            if (myLogger == nullptr)
+                myLogger = std::shared_ptr<Logger>(new Logger(level));
+            else if (myLogger->myLevel != level)
+                myLogger->setLevel(level);
+            return myLogger;
+        }
 
-		/***********************************************************
-		 *  Destroy the logger. 
-		 ***********************************************************/
-		static void destroyLogger() {
-			myLogger = nullptr;
-		}
-		
-		/***********************************************************
-		 *  Destructor. 
-		 ***********************************************************/
-		~Logger() {
-			// Stop the logger and close the file stream.
-			stop();
-			if (logFileStream) {
-				logFileStream->close();
-				logFileStream = nullptr;
-			}
-		}
+        /***********************************************************
+         *  Destroy the logger. 
+         ***********************************************************/
+        static void destroyLogger() {
+            myLogger = nullptr;
+        }
+        
+        /***********************************************************
+         *  Destructor. 
+         ***********************************************************/
+        ~Logger() {
+            // Stop the logger and close the file stream.
+            stop();
+            if (logFileStream) {
+                logFileStream->close();
+                logFileStream = nullptr;
+            }
+        }
 
-		/***********************************************************
-		 *  Add log record to the log queue.
-		 ***********************************************************/
-		void write(Level level, const LogMessage& logMsg) {
-			if (level > myLevel)
-				return;
+        /***********************************************************
+         *  Add log record to the log queue.
+         ***********************************************************/
+        void write(Level level, const LogMessage& logMsg) {
+            if (level > myLevel)
+                return;
 
-			// Create log record
-			std::stringstream logRecordStream;
-			logRecordStream << getCurrentTime()
-				<< " [" << levelToString(level) << "] "
-				<< logMsg;
+            // Create log record
+            std::stringstream logRecordStream;
+            logRecordStream << getCurrentTime()
+                << " [" << levelToString(level) << "] "
+                << logMsg;
 
-			// Add the log record to log queue
-			logQueue.put(logRecordStream.str());
-		}
-		
-		/***********************************************************
-		 *  Set the threshold for this logger. Logging messages which 
-		 *  are less severe than this level will be ignored.
-		 ***********************************************************/
-		void setLevel(Level level) {
-			myLevel = level;
-		}
+            // Add the log record to log queue
+            logQueue.put(logRecordStream.str());
+        }
+        
+        /***********************************************************
+         *  Set the threshold for this logger. Logging messages which 
+         *  are less severe than this level will be ignored.
+         ***********************************************************/
+        void setLevel(Level level) {
+            myLevel = level;
+        }
 
-		/***********************************************************
-		 *  Get the threshold for this logger.
-		 ***********************************************************/
-		Level getLevel() const {
-			return myLevel;
-		}
-		
-		/***********************************************************
-		 *  Get the threshold for this logger as a string.
-		 ***********************************************************/
-		const std::string getLevelString() const {
-			levelToString(myLevel);
-		}
+        /***********************************************************
+         *  Get the threshold for this logger.
+         ***********************************************************/
+        Level getLevel() const {
+            return myLevel;
+        }
+        
+        /***********************************************************
+         *  Get the threshold for this logger as a string.
+         ***********************************************************/
+        const std::string getLevelString() const {
+            levelToString(myLevel);
+        }
 
-		/***********************************************************
-		 *  Stop the logger.
-		 ***********************************************************/
-		void stop() {
-			if (!needToStop) {
-				needToStop = true;
+        /***********************************************************
+         *  Stop the logger.
+         ***********************************************************/
+        void stop() {
+            if (!needToStop) {
+                needToStop = true;
 
-				// Stop the log queue
-				logQueue.stop();
+                // Stop the log queue
+                logQueue.stop();
 
-				// Wait until all log records are written to file
-				writeThread->join();
-				writeThread = nullptr;
-			}
-		}
+                // Wait until all log records are written to file
+                writeThread->join();
+                writeThread = nullptr;
+            }
+        }
 
-		/***********************************************************
-		 *  If the logger was stopped, restart it.
-		 ***********************************************************/
-		void start() {
-			if (needToStop) {
-				needToStop = false;
-				writeThread = std::make_shared<std::thread>([this] { runWriteThread(); } );
-				logQueue.start();
-			}
-		}
+        /***********************************************************
+         *  If the logger was stopped, restart it.
+         ***********************************************************/
+        void start() {
+            if (needToStop) {
+                needToStop = false;
+                writeThread = std::make_shared<std::thread>([this] { runWriteThread(); } );
+                logQueue.start();
+            }
+        }
 
-		/***********************************************************
-		 *  Set the log file path where the log records are send to.
-		 ***********************************************************/
-		bool setLogFilePath(const std::string& logFilePath) {
-			std::lock_guard<std::mutex> lock(myMutex);
-			if (myLogFilePath == logFilePath)
-				return true;
-			myLogFilePath = logFilePath;
-			return setFileStream(logFilePath);
-		}
-		
-		/***********************************************************
-		 *  Get the log file path where the log records are send to.
-		 ***********************************************************/
-		const std::string getLogFilePath() const {
-			std::lock_guard<std::mutex> lock(myMutex);
-			return myLogFilePath;
-		}
-		
-	private:
-		SynchronousQueue<LogRecord> logQueue;
-		std::atomic<Level> myLevel;
-		std::string myLogFilePath;
-		std::shared_ptr<std::ofstream> logFileStream;
-		std::shared_ptr<std::thread> writeThread;
-		std::atomic<bool> needToStop;
-		mutable std::mutex myMutex;
-		static std::shared_ptr<Logger> myLogger;
+        /***********************************************************
+         *  Set the log file path where the log records are send to.
+         ***********************************************************/
+        bool setLogFilePath(const std::string& logFilePath) {
+            std::lock_guard<std::mutex> lock(myMutex);
+            if (myLogFilePath == logFilePath)
+                return true;
+            myLogFilePath = logFilePath;
+            return setFileStream(logFilePath);
+        }
+        
+        /***********************************************************
+         *  Get the log file path where the log records are send to.
+         ***********************************************************/
+        const std::string getLogFilePath() const {
+            std::lock_guard<std::mutex> lock(myMutex);
+            return myLogFilePath;
+        }
+        
+    private:
+        SyncQueue<LogRecord> logQueue;
+        std::atomic<Level> myLevel;
+        std::string myLogFilePath;
+        std::shared_ptr<std::ofstream> logFileStream;
+        std::shared_ptr<std::thread> writeThread;
+        std::atomic<bool> needToStop;
+        mutable std::mutex myMutex;
+        static std::shared_ptr<Logger> myLogger;
 
-		/***********************************************************
-		 *  A private constructor. The parameter "level" specifies 
-		 *  the lowest severity that will be dispatched to the log
-		 *  file. The parameter "logFilePath" specifies the log file
-		 *  path where the log records are send to.
-		 ***********************************************************/
-		Logger(Level level, const std::string& logFilePath = "acht_log.log") 
-		: myLevel(level), logQueue(100), myLogFilePath(logFilePath), needToStop(false) {
-			setFileStream(logFilePath);
-			writeThread = std::make_shared<std::thread>([this] { runWriteThread(); } );
-		}
-	
-		/***********************************************************
-		 *  Set file stream with the log file path.
-		 ***********************************************************/
-		bool setFileStream(const std::string& logFilePath) {
-			logFileStream = std::make_shared<std::ofstream>();
-			logFileStream->open(logFilePath, std::ofstream::app);
+        /***********************************************************
+         *  A private constructor. The parameter "level" specifies 
+         *  the lowest severity that will be dispatched to the log
+         *  file. The parameter "logFilePath" specifies the log file
+         *  path where the log records are send to.
+         ***********************************************************/
+        Logger(Level level, const std::string& logFilePath = "acht_log.log") 
+        : myLevel(level), logQueue(100), myLogFilePath(logFilePath), needToStop(false) {
+            setFileStream(logFilePath);
+            writeThread = std::make_shared<std::thread>([this] { runWriteThread(); } );
+        }
+    
+        /***********************************************************
+         *  Set file stream with the log file path.
+         ***********************************************************/
+        bool setFileStream(const std::string& logFilePath) {
+            logFileStream = std::make_shared<std::ofstream>();
+            logFileStream->open(logFilePath, std::ofstream::app);
 
-			if (!logFileStream->is_open()) {
-				std::cerr << "Failed to open log file: " << logFilePath << std::endl; 
-				logFileStream = nullptr;
-				return false;
-			}
+            if (!logFileStream->is_open()) {
+                std::cerr << "Failed to open log file: " << logFilePath << std::endl; 
+                logFileStream = nullptr;
+                return false;
+            }
 
-			return true;
-		}
+            return true;
+        }
 
-		/***********************************************************
-		 *  Run the write thread until logger is stopped.
-		 *  Write every log record it takes from the log queue, 
-		 *  waiting if the task queue is empty.
-		 ***********************************************************/
-		void runWriteThread() {
-			while (!needToStop) {
-				std::string log;
-				if (logQueue.take(log) && logFileStream) {
-					*logFileStream << log << std::endl;
-				}
-			}
-		}
+        /***********************************************************
+         *  Run the write thread until logger is stopped.
+         *  Write every log record it takes from the log queue, 
+         *  waiting if the task queue is empty.
+         ***********************************************************/
+        void runWriteThread() {
+            while (!needToStop) {
+                std::string log;
+                if (logQueue.take(log) && logFileStream) {
+                    *logFileStream << log << std::endl;
+                }
+            }
+        }
 
-		/***********************************************************
-		 *  Convert a level to a string.
-		 ***********************************************************/
-		const std::string levelToString(Level level) const {
-			switch(level) {
-				case Level::FATAL:
-					return "FATAL";
-				case Level::ERROR:
-					return "ERROR";
-				case Level::WARN:
-					return "WARN";
-				case Level::INFO:
-					return "INFO";
-				case Level::DEBUG:
-					return "DEBUG";
-			}		
-		}
+        /***********************************************************
+         *  Convert a level to a string.
+         ***********************************************************/
+        const std::string levelToString(Level level) const {
+            switch(level) {
+                case Level::FATAL:
+                    return "FATAL";
+                case Level::ERROR:
+                    return "ERROR";
+                case Level::WARN:
+                    return "WARN";
+                case Level::INFO:
+                    return "INFO";
+                case Level::DEBUG:
+                    return "DEBUG";
+            }        
+        }
 
-		/***********************************************************
-		 *  Get the current time as a string.
-		 ***********************************************************/
-		const std::string getCurrentTime() const {
-			auto time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-			std::stringstream tStream;
-			tStream << std::put_time(std::localtime(&time), "%Y-%m-%d %X");
-			return tStream.str();
-		}
-	};
+        /***********************************************************
+         *  Get the current time as a string.
+         ***********************************************************/
+        const std::string getCurrentTime() const {
+            auto time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+            std::stringstream tStream;
+            tStream << std::put_time(std::localtime(&time), "%Y-%m-%d %X");
+            return tStream.str();
+        }
+    };
 
-	std::shared_ptr<Logger> Logger::myLogger = nullptr;
+    std::shared_ptr<Logger> Logger::myLogger = nullptr;
 
-	/***********************************************************
-	*  Log messages to the log file.
-	***********************************************************/
-	#define LOG_FATAL(LOG_MESSAGE) Logger::getLogger()->write(acht::Logger::Level::FATAL, LOG_MESSAGE);
-	#define LOG_ERROR(LOG_MESSAGE) Logger::getLogger()->write(acht::Logger::Level::ERROR, LOG_MESSAGE);
-	#define LOG_WARN(LOG_MESSAGE) Logger::getLogger()->write(acht::Logger::Level::WARN, LOG_MESSAGE);
-	#define LOG_INFO(LOG_MESSAGE) Logger::getLogger()->write(acht::Logger::Level::INFO, LOG_MESSAGE);
-	#define LOG_DEBUG(LOG_MESSAGE) Logger::getLogger()->write(acht::Logger::Level::DEBUG, LOG_MESSAGE);
+    /***********************************************************
+    *  Log messages to the log file.
+    ***********************************************************/
+    #define LOG_FATAL(LOG_MESSAGE) Logger::getLogger()->write(acht::Logger::Level::FATAL, LOG_MESSAGE);
+    #define LOG_ERROR(LOG_MESSAGE) Logger::getLogger()->write(acht::Logger::Level::ERROR, LOG_MESSAGE);
+    #define LOG_WARN(LOG_MESSAGE) Logger::getLogger()->write(acht::Logger::Level::WARN, LOG_MESSAGE);
+    #define LOG_INFO(LOG_MESSAGE) Logger::getLogger()->write(acht::Logger::Level::INFO, LOG_MESSAGE);
+    #define LOG_DEBUG(LOG_MESSAGE) Logger::getLogger()->write(acht::Logger::Level::DEBUG, LOG_MESSAGE);
 }
 
 #endif
--- acht/ThreadPool.hpp
+++ acht/ThreadPool.hpp
@@ -1,115 +1,115 @@
 #ifndef _THREAD_POOL_HPP_
 #define _THREAD_POOL_HPP_
 
-#include "SynchronousQueue.hpp"
+#include "SyncQueue.hpp"
 #include <vector>
 #include <memory>
 #include <thread>
 #include <atomic>
 
 namespace acht {
-	
-	class ThreadPool {
-	private:
-		using Task = std::function<void()>;
-		std::atomic<bool> shutdown;
-		std::vector<std::shared_ptr<std::thread>> myThreads;
-		SynchronousQueue<Task> myTasks;
-		
-		/***********************************************************
-		 *  Run the thread until the pool is shut down. 
-		 *  Execute every task it takes from the task queue, waiting
-		 *  if the task queue is empty.
-		 ***********************************************************/
-		void run() {
-			while (!shutdown) {
-				Task task;
-				if (myTasks.take(task)) {
-					task();
-				}
-			}
-		}
+    
+    class ThreadPool {
+    private:
+        using Task = std::function<void()>;
+        std::atomic<bool> shutdown;
+        std::vector<std::shared_ptr<std::thread>> myThreads;
+        SyncQueue<Task> myTasks;
+        
+        /***********************************************************
+         *  Run the thread until the pool is shut down. 
+         *  Execute every task it takes from the task queue, waiting
+         *  if the task queue is empty.
+         ***********************************************************/
+        void run() {
+            while (!shutdown) {
+                Task task;
+                if (myTasks.take(task)) {
+                    task();
+                }
+            }
+        }
 
-		/***********************************************************
-		 *  Create worker threads
-		 ***********************************************************/
-		void makeThreads(int numThreads) {
-			for (int i = 0; i < numThreads; ++i) {
-				myThreads.push_back(std::make_shared<std::thread>([this] { run(); } ));
-			}
-		}
+        /***********************************************************
+         *  Create worker threads
+         ***********************************************************/
+        void makeThreads(int numThreads) {
+            for (int i = 0; i < numThreads; ++i) {
+                myThreads.push_back(std::make_shared<std::thread>([this] { run(); } ));
+            }
+        }
 
-	public:
-		/***********************************************************
-		 *  Create a thread pool. Set the number of threads and max
-		 *  tasks number.
-		 ***********************************************************/
-		ThreadPool(int numThreads = std::thread::hardware_concurrency(), int maxTask = 100) 
-		: myTasks(maxTask), shutdown(false) {
-			makeThreads(numThreads);
-		}
-		
-		/***********************************************************
-		 *  Shut down the pool if it wasn't shut down.
-		 ***********************************************************/
-		~ThreadPool() {
-			shutdownNow();
-		}
-		
-		/***********************************************************
-		 *  Submit task(lvalue) to the task queue.
-		 ***********************************************************/
-		void submit(const Task& task) {
-			myTasks.put(task);
-		}
-		
-		/***********************************************************
-		 *  Submit task(rvalue) to the task queue.
-		 ***********************************************************/
-		void submit(Task&& task) {
-			myTasks.put(std::forward<Task>(task));
-		}
+    public:
+        /***********************************************************
+         *  Create a thread pool. Set the number of threads and max
+         *  tasks number.
+         ***********************************************************/
+        ThreadPool(int numThreads = std::thread::hardware_concurrency(), int maxTask = 100) 
+        : myTasks(maxTask), shutdown(false) {
+            makeThreads(numThreads);
+        }
+        
+        /***********************************************************
+         *  Shut down the pool if it wasn't shut down.
+         ***********************************************************/
+        ~ThreadPool() {
+            shutdownNow();
+        }
+        
+        /***********************************************************
+         *  Submit task(lvalue) to the task queue.
+         ***********************************************************/
+        void submit(const Task& task) {
+            myTasks.put(task);
+        }
+        
+        /***********************************************************
+         *  Submit task(rvalue) to the task queue.
+         ***********************************************************/
+        void submit(Task&& task) {
+            myTasks.put(std::forward<Task>(task));
+        }
 
-		/***********************************************************
-		 *  If the pool was shut down, restart it.
-		 ***********************************************************/
-		void start(int numThreads = std::thread::hardware_concurrency(), int maxTask = 100) {
-			if (shutdown) {
-				shutdown = false;
-				makeThreads(numThreads);
-				// Restart the task queue
-				myTasks.start();
-				myTasks.setMaxSize(maxTask);
-			}
-		}
+        /***********************************************************
+         *  If the pool was shut down, restart it.
+         ***********************************************************/
+        void start(int numThreads = std::thread::hardware_concurrency(), int maxTask = 100) {
+            if (shutdown) {
+                shutdown = false;
+                makeThreads(numThreads);
+                // Restart the task queue
+                myTasks.start();
+                myTasks.setMaxSize(maxTask);
+            }
+        }
 
-		/***********************************************************
-		 *  Shut down the pool.
-		 ***********************************************************/
-		void shutdownNow() {
-			if (!shutdown) {
-				shutdown = true;
-				
-				// Stop the task queue
-				myTasks.stop();	
-				
-				// Wait until submitted tasks are finish
-				for (auto thread : myThreads) {
-					if (thread)
-						thread->join();
-				}
-				myThreads.clear();
-			}
-		}
+        /***********************************************************
+         *  Shut down the pool.
+         ***********************************************************/
+        void shutdownNow() {
+            if (!shutdown) {
+                shutdown = true;
+                
+                // Stop the task queue
+                myTasks.stop();    
+                
+                // Wait until submitted tasks are finish
+                for (auto thread : myThreads) {
+                    if (thread)
+                        thread->join();
+                }
+                myThreads.clear();
+            }
+        }
 
-		/***********************************************************
-		 *  Set max tasks number.
-		 ***********************************************************/
-		void setMaxTask(int maxTask) {
-			myTasks.setMaxSize(maxTask);	
-		}
-	};
-	
+        /***********************************************************
+         *  Set max tasks number.
+         ***********************************************************/
+        void setMaxTask(int maxTask) {
+            myTasks.setMaxSize(maxTask);    
+        }
+    };
+    
 }
 
 #endif
