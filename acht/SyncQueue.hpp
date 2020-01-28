03c4ca7bbf5e4025cc03e7b2e22b06afeebd001f
whichxjy <whichxjy@gmail.com>
2020-01-28 09:27:39 +0800

    Update SyncQueue

--- acht/SyncQueue.hpp
+++ acht/SyncQueue.hpp
@@ -7,190 +7,196 @@
 #include <functional>
 
 namespace acht {
-    
+
     template <typename T>
     class SyncQueue {
     private:
-        std::queue<T> myQueue;
-        int queueMaxSize;
-        mutable std::mutex myMutex;
-        std::condition_variable notEmpty;
-        std::condition_variable notFull;
-        bool needToStop;
-        
+        std::queue<T> my_queue;
+        int queue_max_size;
+        mutable std::mutex my_mutex;
+        std::condition_variable not_empty;
+        std::condition_variable not_full;
+        bool need_to_stop;
+
         /***********************************************************
          *  A helper function that adds element to the queue,
          *  waiting if queue is full.
          ***********************************************************/
         template <typename Type>
         void putHelper(Type&& elem) {
-            std::unique_lock<std::mutex> lock(myMutex);
-            while (!needToStop && full()) {
-                notFull.wait(lock);
+            std::unique_lock<std::mutex> lock(my_mutex);
+            while (!need_to_stop && full()) {
+                not_full.wait(lock);
             }
-            if (needToStop)
+            if (need_to_stop) {
                 return;
-            myQueue.emplace(std::forward<Type>(elem));
-            notEmpty.notify_one();
+            }
+            my_queue.emplace(std::forward<Type>(elem));
+            not_empty.notify_one();
         }
 
         /***********************************************************
-         *  Check whether the queue is full without lock.
+         *  Check if the queue is full without lock.
          ***********************************************************/
         bool full() const {
-            return myQueue.size() == queueMaxSize;
+            return my_queue.size() == queue_max_size;
         }
 
         /***********************************************************
-         *  Check whether the queue is empty without lock.
+         *  Check if the queue is empty without lock.
          ***********************************************************/
         bool empty() const {
-            return myQueue.size() == 0;
+            return my_queue.size() == 0;
         }
 
     public:
-        SyncQueue(int maxSize) : queueMaxSize(maxSize), needToStop(false) {
-        }
+        SyncQueue(int maxSize) : queue_max_size(maxSize), need_to_stop(false) {}
 
         ~SyncQueue() {
-            // If the queue wasn't stoped, then stop it. 
-            stop();    
+            // If the queue wasn't stoped, then stop it.
+            stop();
         }
 
         // No copy
         SyncQueue(const SyncQueue&) = delete;
-        
+
         // No assignment
         SyncQueue& operator=(const SyncQueue&) = delete;
-        
+
         /***********************************************************
-         *  Add the specified element to this queue, 
+         *  Add the given element to this queue,
          *  waiting if queue is full.
          ***********************************************************/
         void put(const T& elem) {
             putHelper(elem);
         }
-        
+
         void put(T&& elem) {
             putHelper(std::forward<T>(elem));
         }
-        
+
         /***********************************************************
          *  Retrieve and remove the head of this queue.
-         *  
+         *
          *  There are two modes for this operation: Blocked or not.
-         *  If "Blocking" is true, then wait if the queue is empty.
-         *  If "Blocking" is false, then give up if the queue is empty.
+         *  If "blocking" is true, then wait if the queue is empty.
+         *  If "blocking" is false, then give up if the queue is empty.
          ***********************************************************/
-        bool take(T& elem, bool Blocking = true) {
-            std::unique_lock<std::mutex> lock(myMutex);
-            if (Blocking) {
+        bool take(T& elem, bool blocking = true) {
+            std::unique_lock<std::mutex> lock(my_mutex);
+            if (blocking) {
                 // blocking mode
-                while (!needToStop && empty()) {
-                    notEmpty.wait(lock);
+                while (!need_to_stop && empty()) {
+                    not_empty.wait(lock);
                 }
             }
             else {
                 // non-blocking mode
-                if (empty())
-                    return false; 
+                if (empty()) {
+                    return false;
+                }
             }
-            if (needToStop)
+            if (need_to_stop) {
                 return false;
+            }
             // Take element
-            elem = myQueue.front();
-            myQueue.pop();
-            notFull.notify_one();
+            elem = my_queue.front();
+            my_queue.pop();
+            not_full.notify_one();
             return true;
         }
-        
+
         /***********************************************************
          *  Retrieve and remove all elements of this queue.
-         *  
+         *
          *  There are two modes for this operation: Blocked or not.
-         *  If "Blocking" is true, then wait if the queue is empty.
-         *  If "Blocking" is false, then give up if the queue is empty.
+         *  If "blocking" is true, then wait if the queue is empty.
+         *  If "blocking" is false, then give up if the queue is empty.
          ***********************************************************/
-        bool takeAll(std::queue<T> &otherQueue, bool Blocking = true) {
-            std::unique_lock<std::mutex> lock(myMutex);
-            if (Blocking) {
+        bool takeAll(std::queue<T> &other_queue, bool blocking = true) {
+            std::unique_lock<std::mutex> lock(my_mutex);
+            if (blocking) {
                 // blocking mode
-                while (!needToStop && empty()) {
-                    notEmpty.wait(lock);
+                while (!need_to_stop && empty()) {
+                    not_empty.wait(lock);
                 }
             }
             else {
                 // non-blocking mode
-                if (empty())
-                    return false; 
+                if (empty()) {
+                    return false;
+                }
             }
-            if (needToStop)
+            if (need_to_stop) {
                 return false;
+            }
             // Take all elements
-            otherQueue = std::move(myQueue);
-            notFull.notify_one();
+            other_queue = std::move(my_queue);
+            not_full.notify_one();
             return true;
         }
-        
+
         /***********************************************************
          *  Start queue
          ***********************************************************/
         void start() {
-            std::lock_guard<std::mutex> lock(myMutex);
-            // If the queue was stoped, then start it. 
-            if (needToStop)
-                needToStop = false;
+            std::lock_guard<std::mutex> lock(my_mutex);
+            // If the queue was stoped, then restart it.
+            if (need_to_stop) {
+                need_to_stop = false;
+            }
         }
-        
+
         /***********************************************************
          *  Stop all operations
          ***********************************************************/
         void stop() {
-            std::lock_guard<std::mutex> lock(myMutex);
-            // If the queue wasn't stoped, then stop it. 
-            if (!needToStop) {
-                needToStop = true;
+            std::lock_guard<std::mutex> lock(my_mutex);
+            // If the queue wasn't stoped, then stop it.
+            if (!need_to_stop) {
+                need_to_stop = true;
                 // Unblocks all threads waiting currently
-                notFull.notify_all();
-                notEmpty.notify_all();
+                not_full.notify_all();
+                not_empty.notify_all();
             }
         }
-        
+
         // Get the size of queue
         int getSize() const {
-            std::lock_guard<std::mutex> lock(myMutex);
-            return myQueue.size();
+            std::lock_guard<std::mutex> lock(my_mutex);
+            return my_queue.size();
         }
-        
+
         // Return true is the queue is full.
         bool isFull() const {
-            std::lock_guard<std::mutex> lock(myMutex);
-            return myQueue.size() == queueMaxSize;
+            std::lock_guard<std::mutex> lock(my_mutex);
+            return my_queue.size() == queue_max_size;
         }
-        
+
         // Return true is the queue is empty.
         bool isEmpty() const {
-            std::lock_guard<std::mutex> lock(myMutex);
-            return myQueue.size() == 0;
+            std::lock_guard<std::mutex> lock(my_mutex);
+            return my_queue.size() == 0;
         }
-        
+
         // Get the max size of the queue.
         int getMaxSize() const {
-            std::lock_guard<std::mutex> lock(myMutex);
-            return queueMaxSize;
+            std::lock_guard<std::mutex> lock(my_mutex);
+            return queue_max_size;
         }
-        
+
         // Set the max size of the queue
         void setMaxSize(int maxSize) {
-            std::lock_guard<std::mutex> lock(myMutex);
-            queueMaxSize = maxSize;
+            std::lock_guard<std::mutex> lock(my_mutex);
+            queue_max_size = maxSize;
         }
-        
-        // Clear all the elements
+
+        // Clear all the elements.
         void clear() {
-            std::lock_guard<std::mutex> lock(myMutex);
-            while (!myQueue.empty())
-                myQueue.pop();
+            std::lock_guard<std::mutex> lock(my_mutex);
+            while (!my_queue.empty()) {
+                my_queue.pop();
+            }
         }
 
     };
